dac2b28823bf4a568ef4b88dbfa5b99d60cc1eae
Leonardo Su <leonardo.su@ccc.ufcg.edu.br>
2021-11-03 11:31:28 -0300

    controllers

---
+++ app/controllers/admin.js
@@ -0,0 +1,58 @@
+﻿module.exports.listarNoticias = function (app, req, res) {
+    var connection = app.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+    noticiasModel.getNoticias(function (erro, result) {
+        res.render("./admin/admin", { validacao: {}, noticias: result, noticia: {} });
+    });
+}
+
+module.exports.adiciona_formulario = function (app, req, res) {
+
+    var connection = app.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+    noticiasModel.getNoticias(function (erro, result) {
+        res.render("./admin/form_add_noticia", { validacao: {}, noticias: result, noticia: {} });
+    });
+}
+
+module.exports.noticias_salvar = function (app, req, res) {
+    var noticia = req.body;
+
+    req.assert('titulo', 'Título é obrigatório').notEmpty();
+    req.assert('descricao', 'Descrição é obrigatório').notEmpty();
+    req.assert('link_imagem', 'Link da imagem é obrigatório').notEmpty();
+    req.assert('link', 'Link da noticia é obrigatório').notEmpty();
+
+
+    var erros = req.validationErrors();
+    var connection = app.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+    var noticiasCarregadas = {};
+    noticiasModel.getNoticias(function (erro, result) {
+        noticiasCarregadas = result;
+    });
+
+    if (erros) {
+        res.render("./admin/form_add_noticia", { validacao: erros, noticias: noticiasCarregadas, noticia: noticia });
+        return;
+    }
+
+
+    noticiasModel.salvarNoticia(noticia, function (erro, result) {
+        res.redirect('/noticias');
+    });
+}
+
+module.exports.noticias_deletar = function (app, req, res) {
+
+    var connection = app.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+    var id_noticia = req.query;
+
+    noticiasModel.deletarNoticia(id_noticia, function (erro, result) {
+        res.render("/admin", { noticia: result });        
+    });
+}
---
+++ app/controllers/home.js
@@ -0,0 +1,8 @@
+﻿module.exports.index = function (app, req, res) {
+    var connection = app.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+    noticiasModel.get10UltimasNoticias(function (error, result) {
+        res.render("./home/index", { noticias: result });
+    });
+}
